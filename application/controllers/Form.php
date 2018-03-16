e61cb925e750cbbaf235f67012abcdbb11e70577
zanuar <zchoiruddin@gmail.com>
2018-03-16 15:18:10 +0700

    verif pages

---
+++ application/controllers/Form.php
@@ -0,0 +1,8 @@
+<?php
+class Form extends CI_Controller {
+
+    public function index()
+	{
+		$this->load->view('verif_pages');
+	}
+}
---
+++ application/views/verif_pages.php
@@ -0,0 +1,83 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Verification Page</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/bootstrap/css/bootstrap.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/animate/animate.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/css-hamburgers/hamburgers.min.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/animsition/css/animsition.min.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/select2/select2.min.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/plugins/daterangepicker/daterangepicker.css')?>">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/util.css')?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/main.css')?>">
+<!--===============================================================================================-->
+</head>
+<body>
+
+
+	<div class="container-contact100">
+
+		<div class="wrap-contact100">
+			<form class="contact100-form validate-form">
+				<span class="contact100-form-title">
+					Validation Form
+				</span>
+				<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						<span>
+							<i class=" m-r-6" aria-hidden="true"></i>
+							Verif
+						</span>
+					</button>
+				</div>
+				<div class="container-contact100-form-btn">
+					<button class="contact100-form-btn">
+						<span>
+							<i class=" m-r-6" aria-hidden="true"></i>
+							Detail
+						</span>
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	
+
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('assets/plugins/jquery/jquery-3.2.1.min.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('assets/plugins/animsition/js/animsition.min.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('assets/plugins/bootstrap/js/popper.js')?>"></script>
+	<script src="<?php echo base_url('assets/plugins/bootstrap/js/bootstrap.min.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('assets/plugins/select2/select2.min.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('assets/plugins/daterangepicker/moment.min.js')?>"></script>
+	<script src="<?php echo base_url('assets/plugins/daterangepicker/daterangepicker.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('assets/plugins/countdowntime/countdowntime.js')?>"></script>
+<!--===============================================================================================-->
+	<script src="<?php echo base_url('assets/plugins/jquery/jquery-3.2.1.min.js')?>js/main.js"></script>
+
+
+</body>
+</html>
+</html>
